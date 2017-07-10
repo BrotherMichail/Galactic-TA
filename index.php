5c29403a00b455ef6eeebd12831cf8b76ac8eab5
Brother Michail <michalvotypka@centrum.cz>
2017-07-10 10:40:18 +0200

    just the first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Galactic Travel Agency</title>
+        <link rel="stylesheet" href="main.css" type="text/css" />
+    </head>
+    <body>
+        <div class="wrap">
+            <header>
+                <h1>Galactic Travel Agency</h1>
+                <h2>Your guide to the whole universe</h2>
+                <img src="img/ig.jpg" alt="header image" />
+            </header>
+            <nav>
+                <div class="menu">
+                    <ul class="topmenu">
+                        <li>Select destination</li>
+                        <li><a href="departures.php">Departures</a></li>
+                        <li><a href="search.php">Search</a></li>
+                        <li><a href="info.html">Informations</a></li>
+                        <li><a href="cont.html">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <?php
+            // put your code here
+            ?>
+            <footer>
+                <p>&copy; 2017 Michal Votypka</p>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ search.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Galactic Travel Agency</title>
+        <link rel="stylesheet" href="main.css" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <img src="img/ig.jpg" alt="header image" />
+            <h1>Galactic Travel Agency</h1>
+            <h2>Your guide to the whole universe</h2>
+        </header>
+        <nav>
+            <div class="menu">
+                <ul class="topmenu">
+                    <li>Select destination</li>
+                    <li><a href="departures.php">Departures</a></li>
+                    <li><a href="search.php">Search</a></li>
+                    <li><a href="info.html">Informations</a></li>
+                    <li><a href="cont.html">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
